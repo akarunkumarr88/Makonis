e549a6e8bd896738b803e25a04ec7c1de444bbcc
akarunkumarr88 <106867662+akarunkumarr88@users.noreply.github.com>
2022-06-13 20:14:47 +0530

    Add files via upload

---
+++ Assignment_Makinos_Api_Updated/Assignment_Makinos_Api/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_Makinos_Api.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
